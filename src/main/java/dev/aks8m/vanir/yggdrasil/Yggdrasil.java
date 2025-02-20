124c1045e9f663caf819b4fa236d2fe48358fa71
Andrew Sills <andrew.k.sills@gmail.com>
2025-02-19 22:08:01 -0500

    working initial implementation

--- src/main/java/dev/aks8m/vanir/yggdrasil/Yggdrasil.java
+++ src/main/java/dev/aks8m/vanir/yggdrasil/Yggdrasil.java
@@ -2,25 +2,65 @@ package dev.aks8m.vanir.yggdrasil;
 
 
 import io.javalin.Javalin;
-import io.javalin.http.staticfiles.Location;
+import io.javalin.websocket.WsBinaryMessageContext;
+import io.javalin.websocket.WsMessageContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
 
 public class Yggdrasil {
 
     private static final Logger LOG = LoggerFactory.getLogger(Yggdrasil.class);
 
-    public void start (int port) {
-        Javalin.create(javalinConfig -> {
-//            javalinConfig.router.mount(router -> {
+    private final Javalin server;
+    private final ByteBuffer receivedBytes = ByteBuffer.allocate(1024);
 
-//                router.ws("/ygg", ws -> {
-//                    ws.onConnect(ctx -> LOG.info("Connected to the chat server"));
-//                    ws.onClose(ctx -> LOG.info("Disconnected from the chat server"));
-//                    ws.onMessage(ctx -> LOG.info("Received message from the chat server"));
-//                });
-//            });
-        }).get("/", ctx -> ctx.result("Hello World"))
-                .start(7070);
+    public Yggdrasil() {
+        server = Javalin.create();
+
+        server.ws("/ygg", ws -> {
+            ws.onConnect(ctx -> {
+                LOG.info("Connected to yggdrasil server");
+                ctx.session.setIdleTimeout(Duration.ZERO);
+                ctx.session.getPolicy().setMaxBinaryMessageSize(1024 * 1024 *1024);
+            });
+            ws.onMessage(ctx-> {
+                LOG.info("Received message: {}", ctx.message());
+            });
+            ws.onBinaryMessage(ctx -> {
+                LOG.info("Received binary message: " + ctx.data().length / (1024 * 1024) + " MB");
+                ByteBuffer byteBuffer = ByteBuffer.wrap(ctx.data());
+                try {
+                    Path output = Paths.get("/home/aks8m/Downloads", "/ygg.webm");
+                    Files.write(output, byteBuffer.array());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            ws.onClose(ctx -> {
+                LOG.info("Closing connection");
+            });
+            ws.onError(e -> {
+                LOG.error("Error: ", e.error());
+            });
+        });
+    }
+    public void start(int port) {
+        server.start(port);
+    }
+
+    public Javalin getServer() {
+        return server;
+    }
+
+    public static void main(String[] args) {
+        Yggdrasil yggdrasil = new Yggdrasil();
+        yggdrasil.start(7070);
     }
 }
--- src/test/java/dev/aks8m/vanir/yggdrasil/test/YggdrasilIT.java
+++ src/test/java/dev/aks8m/vanir/yggdrasil/test/YggdrasilIT.java
@@ -1,17 +1,18 @@
 package dev.aks8m.vanir.yggdrasil.test;
 
 import dev.aks8m.vanir.yggdrasil.Yggdrasil;
+import io.javalin.testtools.JavalinTest;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class YggdrasilIT {
 
+
     @Test
     public void simpleAppTest() {
 
-        Yggdrasil app = new Yggdrasil();
-        app.start(7070);
     }
 
 }
